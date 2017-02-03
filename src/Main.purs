df46a0fb20c79972d5de61ea531d0e91095a8716
aratama <aratama@example.com>
2017-02-04 03:03:38 +0900

    refactor

--- src/Main.purs
+++ src/Main.purs
@@ -1,5 +1,6 @@
 module Main where
 
+import Control.Category ((<<<))
 import Control.Applicative (pure)
 import Control.Bind (class Bind, bind, (>>=))
 import Control.Monad.Aff (Aff, attempt)
@@ -8,8 +9,9 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Exception (error)
 import Control.Monad.Error.Class (throwError)
 import Control.Monad.Except (runExcept)
+import Control.Monad.State (modify)
 import Control.Monad.State.Class (class MonadState, get)
-import Data.Either (Either(..))
+import Data.Either (Either(..), either)
 import Data.Foreign (Foreign)
 import Data.Foreign.Class (readProp)
 import Data.Maybe (Maybe(..))
@@ -27,10 +29,13 @@ import Halogen.HTML.Core (HTML)
 import Halogen.HTML.Elements (br, button, div, h2, img)
 import Halogen.HTML.Events (input_, onClick)
 import Halogen.HTML.Properties (src)
-import Halogen.Query (action, put)
+import Halogen.Query (action)
 import Halogen.VDom.Driver (runUI)
 import Network.HTTP.Affjax (AJAX)
 import Network.HTTP.Affjax (get) as Ajax
+
+loading :: String
+loading = "loading.svg"
 
 type State = {
     topic :: String,
@@ -52,6 +57,7 @@ render model = div [] [
 eval :: forall m eff. (MonadAff (Effects eff) m, MonadState State m, Bind m) => Query ~> m
 eval msg = case msg of
     MorePlease next -> do
+        modify _ { gifUrl = loading }
         model <- get
         let url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" <> model.topic
         response <- liftAff (attempt (Ajax.get url))
@@ -59,7 +65,7 @@ eval msg = case msg of
             Left err -> pure next
             Right res -> do
                 newUrl <- liftAff (decodeGifUrl res.response)
-                put model { gifUrl = newUrl }
+                modify _ { gifUrl = newUrl }
                 pure next
 
   where
@@ -67,9 +73,7 @@ eval msg = case msg of
     decodeGifUrl :: Foreign -> Aff (Effects eff) String
     decodeGifUrl value = do
         let parsed = readProp "data" value >>= readProp "image_url"
-        case runExcept parsed of
-            Left err -> throwError (error (show err))
-            Right url -> pure url
+        either (throwError <<< error <<< show) pure (runExcept parsed)
 
 ui :: forall i o m eff. (MonadAff (Effects eff) m) => Component HTML Query i o m
 ui = component {
@@ -80,7 +84,7 @@ ui = component {
 }
   where
     init :: String -> State
-    init topic = { topic, gifUrl: "waiting.gif" }
+    init topic = { topic, gifUrl: loading }
 
 main :: forall eff. Eff (Effects eff) Unit
 main = runHalogenAff do
